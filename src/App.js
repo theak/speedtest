8809a9ff4636daab89c61da532aefd4c549fcbd0
Akshay Kannan <akshayk@google.com>
2021-07-02 12:02:27 -0700

    basic app works

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="half"><h1>Test</h1></div>
-      <div className="bottom half"><textarea></textarea></div>
-    </div>
-  );
+const prompts = [
+  {
+    "html": "Testy",
+    "text": "Test"
+  }
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPrompt: 0,
+      debug: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({debug: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="half">
+          <h1>{prompts[this.state.currentPrompt].html}</h1>
+          <h3>{this.state.debug}</h3>
+        </div>
+        <div className="bottom half">
+          <textarea onChange={this.handleChange}></textarea>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
