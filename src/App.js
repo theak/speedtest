2cd0d616d4b2094e548ee1b6dc74523cf1da41af
Akshay Kannan <akshayk@google.com>
2021-07-07 11:25:18 -0700

    added support for clearing prompts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 
 import './App.css';
@@ -9,9 +9,8 @@ function stripHtml(html) {
 }
 
 const prompts = [
-  {
-    "html": <span>T<b>e</b>st</span>
-  }
+  {html: <span>T<b>e</b>st</span>, clear: true},
+  {html: <p>Hello</p>, clear: false}
 ];
 
 for (var i = 0; i < prompts.length; i++) {
@@ -33,13 +32,16 @@ class App extends React.Component {
   }
 
   handleChange(event) {
-    if (event.target.value == prompts[this.state.currentPrompt].text)
+    if (this.state.currentPrompt === -1) return;
+    if (event.target.value === prompts[this.state.currentPrompt].text)
       this.setState({taskCompleted: true, taskTimes: [...this.state.taskTimes, Date.now() - this.state.taskStartTime]});
     //this.setState({debug: event.target.value});
     //[...this.state.taskTimes, Date.now() - this.state.taskStartTime]
   }
 
   handleNext(event) {
+    if (this.state.currentPrompt === -1 || prompts[this.state.currentPrompt].clear)
+      document.getElementById("textbox").value = "";
     this.setState({currentPrompt: this.state.currentPrompt + 1, 
       taskCompleted: false, taskStartTime: Date.now()})
   }
@@ -61,7 +63,7 @@ class App extends React.Component {
             <h1>{this.state.taskCompleted ? next : ''}</h1>
           </div>
           <div className="bottom half">
-            <textarea onChange={this.handleChange}></textarea>
+            <textarea id="textbox" onChange={this.handleChange}></textarea>
           </div>
         </div>
       );
@@ -70,7 +72,7 @@ class App extends React.Component {
       <div className="App">
         <h1>Done- please copy the information below, enter your participant ID, then tap submit:</h1>
         <form action="https://docs.google.com/forms/d/e/1FAIpQLSdDzSs-JBDvNuYW225c3Dog4grp0oCIdLfWzQyM0egu6Cf6mg/formResponse" target="_self" method="POST" id="mG61Hd" jsmodel="TOfxwf Q91hve" data-response="%.@.[]]" data-first-entry="0" data-last-entry="1" data-is-first-page="true">
-          <textarea readonly name="entry.1431434204" onFocus={this.handleFocus}>{JSON.stringify(this.state.taskTimes)}</textarea>
+          <textarea readOnly name="entry.1431434204" onFocus={this.handleFocus} value={JSON.stringify(this.state.taskTimes)}></textarea>
           <input name="entry.890606469" placeholder="Enter your participant ID here then tap Submit" />
           <input name="entry.26542893" type="hidden" value={navigator.userAgent} />
           <br/><input type="submit" value="Submit"/>
@@ -81,7 +83,7 @@ class App extends React.Component {
 }
 
 function Next(props) {
-  const text = (props.currentPrompt == -1) ? "Tap to start" : "Next";
+  const text = (props.currentPrompt === -1) ? "Tap to start" : "Next";
   return <button onClick={props.onClick} type="button">{text}</button>
 }
 
